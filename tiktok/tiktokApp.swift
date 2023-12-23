2212502935814afab94aea3f89de906d4a604e52
Arafullah Shihab <arafullah.shihab@aptos.com>
2023-12-23 18:04:26 -0500

    Initial Commit

---
+++ tiktok/tiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokApp.swift
+//  tiktok
+//
+//  Created by Arafullah Shihab on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct tiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
